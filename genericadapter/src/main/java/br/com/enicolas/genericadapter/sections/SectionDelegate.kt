b8b9bb71d5209dcd231e8f496e7e5f807d15e337
Emmanouil Nicolas <emmanouil.nicolas@afya.com.br>
2021-04-13 14:45:31 -0300

    Changed header to be optional

--- genericadapter/src/main/java/br/com/enicolas/genericadapter/sections/SectionDelegate.kt
+++ genericadapter/src/main/java/br/com/enicolas/genericadapter/sections/SectionDelegate.kt
@@ -8,8 +8,8 @@ interface SectionDelegate {
     fun numberOfSections(): Int { return 1 }
     fun numberOfRowsInSection(section: Int): Int { return 0 }
     fun cellForRowAt(indexPath: IndexPath, cell: RecyclerView.ViewHolder)
-    fun didSelectRowAt(indexPath: IndexPath)
+    fun didSelectRowAt(indexPath: IndexPath) {}
     fun registerCellAt(indexPath: IndexPath): AdapterHolderType?
-    fun registerHeaderForSection(section: Int): AdapterHolderType?
-    fun viewForHeaderInSection(section: Int, header: RecyclerView.ViewHolder)
+    fun registerHeaderForSection(section: Int): AdapterHolderType? { return null }
+    fun viewForHeaderInSection(section: Int, header: RecyclerView.ViewHolder) {}
 }
